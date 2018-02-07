b40b314f07e60c490e0e283a2bf3ffb823a25d63
一叶知秋 <378706324@qq.com>
2018-02-07 14:05:55 +0800

    1 添加相册和相机 2 处理配置刷新和加载库

--- lib_common/src/main/java/com/yj/njh/common/app/ComApp.java
+++ lib_common/src/main/java/com/yj/njh/common/app/ComApp.java
@@ -1,17 +1,48 @@
 package com.yj.njh.common.app;
 
+import android.content.Context;
+
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.Utils;
 import com.jkb.fragment.rigger.rigger.Rigger;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreater;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreater;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
+import com.scwang.smartrefresh.layout.header.ClassicsHeader;
 import com.yj.njh.base.base.app.XBaseApp;
 import com.yj.njh.ret.http.retrofit.TokenManager;
+
+import action.njh.yj.com.lib_common.R;
 
 /**
  * Created by niejiahuan on 18/2/3.
  */
 
 public class ComApp extends XBaseApp {
+    //static 代码段可以防止内存泄露
+    static {
+        //设置全局的Header构建器
+        SmartRefreshLayout.setDefaultRefreshHeaderCreater(new DefaultRefreshHeaderCreater() {
+            @Override
+            public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
+                layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
+                return new ClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
+            }
+        });
+        //设置全局的Footer构建器
+        SmartRefreshLayout.setDefaultRefreshFooterCreater(new DefaultRefreshFooterCreater() {
+            @Override
+            public RefreshFooter createRefreshFooter(Context context, RefreshLayout layout) {
+                //指定为经典Footer，默认是 BallPulseFooter
+                return new ClassicsFooter(context).setDrawableSize(20);
+            }
+        });
+    }
     @Override
     public void onCreate() {
         super.onCreate();
